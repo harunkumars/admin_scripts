31ad8dc616819b173eaecebe049bf6fb15307bf0
harunkumars <harun@betacraft.io>
2023-01-30 10:10:37 +0530

    Add Minimal functional execute script action

--- app/admin/active_admin_reports.rb
+++ app/admin/active_admin_reports.rb
@@ -1,18 +1,11 @@
 ActiveAdmin.register ActiveAdminReport do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description, :ruby_script
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :ruby_script]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :name, :description, :ruby_script
+
+  action_item :execute, only: :show do
+    link_to 'Execute Ruby Script', execute_admin_active_admin_report_path(resource)
+  end
+  member_action :execute do
+    render plain: eval(resource.ruby_script)
+  end
 end
